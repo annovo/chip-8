67dbad59990d69104050a16e19fb710d145716a8
Anastasiia Novoselova <ananas.nasya@gmail.com>
2021-11-09 19:34:28 -0800

    minor change

--- src/Canvas.js
+++ src/Canvas.js
@@ -1,4 +1,4 @@
-import {useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Chip8 from "./Chip";
 
 const Canvas = ({ file, keyPressed, setKey }) => {
